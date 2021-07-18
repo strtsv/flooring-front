f50ac6a2321b602f12b4778de795e96dbe2e455a
jsusilo <enr@bk.ru>
2021-07-18 21:06:01 +0500

    SingleItem Welcome

---
+++ src/components/SingleItem/Welcome.js
@@ -0,0 +1,51 @@
+import React from "react";
+
+class Welcome extends React.Component {
+  render() {
+    return (
+      <section className="section section-sm bg-default">
+        <div className="container">
+          <h2>Special Offers</h2>
+          <div className="row row-20">
+            <div className="col-md-6">
+              <p>
+                Lorem ipsum dolor sit amet conse ctetur adipisicing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                sunt in culpa qui officia deserunt. Lorem ipsum dolor sit amet
+                conse ctetur adipisicing elit, sed do eiusmod tempor incididunt
+                ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+                nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+                commodo consequat.
+              </p>
+            </div>
+            <div className="col-md-6">
+              <p>
+                Ipsum dolor sit amet conse ctetur adipisicing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                sunt in culpa qui officia deserunt. Lorem ipsum dolor sit amet
+                conse ctetur adipisicing elit, sed do eiusmod tempor incididunt
+                ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+                nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+                commodo consequat.
+                <a
+                  className="link-arrow icon novi-icon icon-lg fa fa-angle-right"
+                  href="#"
+                />
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+    );
+  }
+}
+
+export default Welcome;
